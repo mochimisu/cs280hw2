0b7ca1fb4a3ee0f313b125479cdec7e0c22f7125
Brandon Wang <auraxiii@gmail.com>
2012-02-27 19:15:14 -0800

    q3 exec

--- phog.m
+++ phog.m
@@ -9,12 +9,12 @@ function features = phog_helper(image_horiz, image_vert, window_size)
   [width, height] = size(image_horiz);
   overlap = floor(window_size / 2);
   features = zeros(9, 1, floor((width - window_size) / overlap), floor((height - window_size) / overlap));
-  for x = 1:overlap_size:(width - window_size)
-    for y = 1:overlap_size:(height - window_size)
-      xrange = x : x + window_size;
-      yrange = y : y + window_size;
-      window_horiz = image_horiz(xrange, yrange);
-      window_vert = image_vert(xrange, yrange);
+  for x = 1:overlap:(width - window_size)
+    for y = 1:overlap:(height - window_size)
+      xrange = x : (x + window_size - 1);
+      yrange = y : (y + window_size - 1);
+      window_horiz = image_horiz(yrange, xrange);
+      window_vert = image_vert(yrange, xrange);
       features(:, :, x, y) = count_orientations(window_horiz, window_vert);
     end
   end
@@ -26,9 +26,9 @@ function hist = count_orientations(window_horiz, window_vert)
   orientations = floor(orientations * (2 * pi / 9));
   results = zeros(9, 1);
   for i = 1:9
-    results(i) = sum(orientations == i - 1);
+    results(i) = sum(orientations(:) == (i - 1));
   end
-  results = results / sum(results);
+  hist = results / sum(results);
 end
 
 function [hr, vr] = responses(image)
---
+++ q3.m
@@ -0,0 +1,26 @@
+function results = q3(use_full)
+  if nargin < 1
+    use_full = false;
+  end
+  results = run_svm(@phog_features, use_full);
+  
+  sample_axis = [100 200 500 1000 2000 5000 10000 60000];
+  error_max = [1 1 1 1 1 1 1 1];
+  if not(use_full)
+    sample_axis = sample_axis(1:7);
+    error_max = error_max(1:7);
+  end
+  
+  error = error_max - results/10000;
+      
+  plot(sample_axis,error);
+end
+
+function features = phog_features(images)
+    new_size = 8937;
+    max = size(images,3);
+    features = zeros(new_size, max);
+    for i = 1:max
+        features(:,i) = double(phog(images(:,:,i)));
+    end;
+end
